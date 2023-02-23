a7b73a828a3df1db4d8c04a3e471baa510f5d9f3
onedus <onedous@gmail.com>
2023-02-23 16:23:45 +0800

    updated.

--- CopyScript.bat
+++ CopyScript.bat
@@ -1,14 +1,31 @@
 @echo off
 
-set runtime_assets=D:\code\NPLRuntime\NPLRuntime\Platform\AndroidStudio\app\assets
+set sdk_path=C:\\Users\\onedou\\ParacraftAndroidDev
+set dest_path=D:\\code\\NPLRuntime\\NPLRuntime\\Platform\\AndroidStudio\\app\\assets
+set paracraft_build_in_mod_path=D:\\code\\ParacraftBuildinMod
+set dev=C:\\Users\\onedou\\dev
+set trunk_path=%dev%\\trunk
+set paraengine=D:\\code\\ParaCraftSDK\\redist\\ParaEngineClient.exe
+set boost_file=%sdk_path%\create_pkg.lua
 
-del %runtime_assets%\npl_packages\ParacraftBuildinMod.zip
-del %runtime_assets%\main.pkg
-del %runtime_assets%\main150727.pkg
-del %runtime_assets%\assets_manifest.txt
-@REM rm %runtime_assets%\version.txt
+pushd %trunk_path%
+%sdk_path%\7z.exe a %dev%\main.zip .\ -xr!.git
+popd
 
-curl -o %runtime_assets%\npl_packages\ParacraftBuildinMod.zip http://10.8.0.2/ParacraftBuildinMod.zip
-curl -o %runtime_assets%\main.pkg http://10.8.0.2/main.pkg
-curl -o %runtime_assets%\main150727.pkg http://10.8.0.2/main_dev.pkg
-curl -o %runtime_assets%\assets_manifest.txt http://10.8.0.2/assets_manifest.txt._P_E_0
+echo %boost_file%
+
+echo NPL.load("(gl)script/ide/commonlib.lua");>%boost_file%
+echo local filesPath = "%dev%";>>%boost_file%
+echo local destPath = "%dest_path%";>>%boost_file%
+echo local originFile = "%dev%\\main.zip";>>%boost_file%
+echo local encryptFile = "%dest_path%\\main.pkg";>>%boost_file%
+echo ParaAsset.GeneratePkgFile(originFile, encryptFile);>>%boost_file%
+echo -- ParaIO.CopyFile(encryptFile, destPath .. "\\main.pkg", true);>>%boost_file%
+echo ParaIO.DeleteFile(originFile);>>%boost_file%
+echo --ParaIO.DeleteFile(encryptFile);>>%boost_file%
+echo ParaIO.DeleteFile(filesPath .. "\\create_pkg.lua");>>%boost_file%
+echo ParaGlobal.ExitApp();>>%boost_file%
+
+call "%paraengine%" bootstrapper="%boost_file%"
+
+del %boost_file%
